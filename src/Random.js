d92cf615c37a976a90cc67be94212229b50b9d58
Dallin Bland <>
2021-04-03 02:48:39 +0000

    added a lyrics function

--- src/Random.js
+++ src/Random.js
@@ -1,6 +1,6 @@
 const { chatBot } = require('reconlx');
 
-_chatbot (message,input) {
+chatbot (message,input) {
     if(!message) throw new TypeError("No message or message.channel was provided")
     if(!input) throw new TypeError("No input (input to give) was provided")
   chatBot(message, input);
